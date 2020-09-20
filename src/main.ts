4f61bbd0a0c7c6e64749291241efa3862443fc5c
Michael O'Sullivan <squirrels@euler.indigoibex.asuscomm.com>
2020-09-20 16:11:32 -0600

    same

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-
+import 'vue-material/dist/vue-material.min.css';
 createApp(App)
   .use(router)
+  // .use(VueMaterial.MdButton)
   .mount('#app');
